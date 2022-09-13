77d89786832c1fcc3ab4e5ef78f120ae63894ee4
MacraeSmith <99697319+MacraeSmith@users.noreply.github.com>
2022-09-12 23:25:18 -0500

    Made Snails Upgrade over time

--- ImmortalSnail_UnityProject/Assets/Scripts/AI/Snail_AI.cs
+++ ImmortalSnail_UnityProject/Assets/Scripts/AI/Snail_AI.cs
@@ -12,19 +12,24 @@ public class Snail_AI : MonoBehaviour
 
     public Vector3 walkPoint;
     bool walkPointSet;
+    public float walkPointRange = 10f;
 
-    private float attackRange = 3f;
+    public float sightRange = 20f, attackRange = 3f;
+
     private bool playerInAttackRange, playerInSightRange;
 
     public float timeBetweenAttacks = .5f;
     bool alreadyAttacked;
 
-    public float walkPointRange = 200f;
+    public int snailLevel = 1;
+    public float snailUpgradeSpeed = 5f;
+
 
 
 
     void Start()
     {
+        StartCoroutine(SnailUpgrade());
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
         //agent.SetDestination(player.position);
@@ -33,20 +38,25 @@ public class Snail_AI : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        SnailStats();
 
-        
-        transform.LookAt(player);
+
+
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
        
        
-        
-        if(!playerInAttackRange)
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patroling();
+        }
+        else if(playerInSightRange &&!playerInAttackRange)
         {
             ChasePlayer();
 
         }
 
-        else if (playerInAttackRange)
+        else if (playerInSightRange && playerInAttackRange)
         {
             AttackPlayer();
         }
@@ -55,16 +65,17 @@ public class Snail_AI : MonoBehaviour
     private void ChasePlayer() //ENEMY SEES PLAYER AND MOVES TOWARDS HIM. SPEED INCREASES
     {
        this.anim.SetBool("isMoving", true);
-       agent.SetDestination(player.position);
+        transform.LookAt(player);
+
+        agent.SetDestination(player.position);
 
 
-        agent.speed = .5f;
     }
 
     private void AttackPlayer() //ENEMY ATTACKS PLAYER
     {
         float distance = Vector3.Distance(transform.position, player.position);
-        if (distance < (attackRange - 1))
+        if (distance < (attackRange - .5f))
         {
             //this.anim.SetBool("Walk Backward Fast", true);
             //this.anim.SetBool("Walk Forward Fast", false);
@@ -103,5 +114,97 @@ public class Snail_AI : MonoBehaviour
         alreadyAttacked = false;
         //Stinger.SetActive(false);
     }
-    
+
+    private void SnailStats()
+    {
+        if(snailLevel == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            agent.speed = .5f;
+            sightRange = 10f;
+            attackRange = 1f;
+        }
+        else if(snailLevel == 2)
+        {
+            transform.localScale = new Vector3(2, 2, 2);
+            agent.speed = 1f;
+            sightRange = 20f;
+            attackRange = 3f;
+
+        }
+        else if(snailLevel == 3)
+        {
+            transform.localScale = new Vector3(3, 3, 3);
+
+            agent.speed = 2f;
+            sightRange = 50f;
+            attackRange = 3f;
+        }
+        else if(snailLevel == 4)
+        {
+            transform.localScale = new Vector3(4, 4, 4);
+
+            agent.speed = 5f;
+            sightRange = 1000f;
+            attackRange = 3f;
+        }
+        else
+        {
+            snailLevel = 4;
+
+        }
+    }
+    private void Patroling()//ENEMY WALKS AROUND 
+    {
+        
+        if (!walkPointSet) SearchWalkPoint();
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude <= 1f)
+        {
+            walkPointSet = false;
+            
+        }
+       
+
+
+        
+    }
+
+    private void SearchWalkPoint() //ENEMY FINDS WHERE TO WALK TO
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, Ground))
+        {
+
+                walkPointSet = true;
+
+        }
+
+       
+
+    }
+    IEnumerator SnailUpgrade()
+    {
+        while (snailLevel <= 4)
+        {
+            
+            yield return new WaitForSeconds(snailUpgradeSpeed);
+            snailLevel++;
+
+        }
+
+
+    }
+
 }
