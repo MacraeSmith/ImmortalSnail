b5d2817e5ad144759ec07f983e3f4b727da9d8cc
twalker47 <91645389+twalker47@users.noreply.github.com>
2022-09-16 14:16:22 -0500

    Added Pause screen to main game scene

---
+++ ImmortalSnail_UnityProject/Assets/Scripts/SceneUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneUI : MonoBehaviour
+{
+
+    public string newGameScene;
+    public GameObject controlsWindow;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+
+    public void OpenControlsWindow()
+    {
+        controlsWindow.SetActive(true);
+    }
+
+    public void CloseControlsWindow()
+    {
+        controlsWindow.SetActive(false);
+    }
+
+}
